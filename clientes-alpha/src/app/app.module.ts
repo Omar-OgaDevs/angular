c91beeb9585c9a9f847da75ff0c882fe688f43db
omar.ogadevs <omar.ogadevs@outlook.com>
2021-06-17 08:16:56 -0500

    add componentes

--- clientes-alpha/src/app/app.module.ts
+++ clientes-alpha/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabeceroComponent } from './core/cabecero/cabecero.component';
+import { TableroComponent } from './core/tablero/tablero.component';
+import { ClientesComponent } from './core/clientes/clientes.component';
+import { EditarClienteComponent } from './core/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegistroComponent } from './core/registro/registro.component';
+import { ConfiguracionComponent } from './core/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './core/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './core/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
