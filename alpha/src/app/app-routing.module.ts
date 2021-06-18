2e4ffe53b8df1328d341c532ba66873734fad163
omar.ogadevs <omar.ogadevs@outlook.com>
2021-06-18 14:46:34 -0500

    routing

--- alpha/src/app/app-routing.module.ts
+++ alpha/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationComponent } from './core/configuration/configuration.component';
+import { EditComponent } from './core/edit/edit.component';
+import { LoginComponent } from './core/login/login.component';
+import { MainComponent } from './core/main/main.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { RegisterComponent } from './core/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'configuration', component: ConfigurationComponent},
+  {path: 'edit/:id', component: EditComponent},
+  {path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
