2c6d2b2ddc334c777219db056c13139d2a709a41
omar.ogadevs <omar.ogadevs@outlook.com>
2021-06-18 14:35:40 -0500

    add components

--- alpha/src/app/app.module.ts
+++ alpha/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { MainComponent } from './core/main/main.component';
+import { ListclientsComponent } from './core/listclients/listclients.component';
+import { EditComponent } from './core/edit/edit.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+import { ConfigurationComponent } from './core/configuration/configuration.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ListclientsComponent,
+    EditComponent,
+    LoginComponent,
+    RegisterComponent,
+    ConfigurationComponent,
+    NotfoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
