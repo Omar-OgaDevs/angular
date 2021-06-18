c265ecbd5e03e21e7d9d5ce343333ea8e2a1712b
omar.ogadevs <omar.ogadevs@outlook.com>
2021-06-18 16:33:00 -0500

    add import modules

--- alpha/src/environments/environment.ts
+++ alpha/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore: {
+    apiKey: "AIzaSyC1Z4nWqodQHUvNyF0xt1F_08BBf3Ov6JA",
+    authDomain: "control-clientes-fdb25.firebaseapp.com",
+    projectId: "control-clientes-fdb25",
+    storageBucket: "control-clientes-fdb25.appspot.com",
+    messagingSenderId: "566623211311",
+    appId: "1:566623211311:web:f7f293d41c202c8e909bda"
+  }
 };
 
 /*
